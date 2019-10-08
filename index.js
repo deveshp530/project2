8133d50042b9affdfa70ad370d5be4c452ed84fe
deveshp530 <deveshp530@gmail.com>
2019-10-07 22:39:39 -0700

    added css

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(parser.urlencoded({extended: true}))
 app.use(parser.json())
 app.use(methodOverride('_method'))
+app.use('/assets', express.static('public'))
 
 
 app.use('/', artistController)
