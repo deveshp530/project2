17db18dd58276d82a8ee519b141d1eede935ff72
deveshp530 <deveshp530@gmail.com>
2019-10-08 21:45:23 -0700

    more stuff for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose')
 let mongoURI = "";
 
+global.Promise = require('bluebird');
+async function test() {
+  // No unhandled rejection!
+  await Promise.reject(new Error('test'));
+}
+// Prints "false"
+console.log(test().catch(() => {}) instanceof require('bluebird'));
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/favArtist";
 }
 
-mongoose.connect('mongodb://localhost/favArtists',{ useNewUrlParser: true }, () => {
+mongoose.connect('mongodb://localhost/favArtists',{ useNewUrlParser: true,useUnifiedTopology: true,
+useFindAndModify: false}, () => {
     console.log("We connected!!!")
   })
 
