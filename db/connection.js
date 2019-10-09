c7e2145c66a160fe19498faec6d7b0db532f79eb
deveshp530 <deveshp530@gmail.com>
2019-10-09 09:00:56 -0700

    test

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose')
+// let mongoURI = "";
+mongoose.Promise = Promise
+// global.Promise = require('bluebird');
+// async function test() {
+//   // No unhandled rejection!
+//   await Promise.reject(new Error('test'));
+// }
+// // Prints "false"
+// console.log(test().catch(() => {}) instanceof require('bluebird'));
 let mongoURI = "";
-
-global.Promise = require('bluebird');
-async function test() {
-  // No unhandled rejection!
-  await Promise.reject(new Error('test'));
-}
-// Prints "false"
-console.log(test().catch(() => {}) instanceof require('bluebird'));
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/favArtist";
 }
 
-mongoose.connect('mongodb://localhost/favArtists',{ useNewUrlParser: true,useUnifiedTopology: true,
-useFindAndModify: false}, () => {
+mongoose.connect(mongoURI,{ useNewUrlParser: true}, () => {
     console.log("We connected!!!")
   })
 
-mongoose.Promise = Promise
 module.exports = mongoose
